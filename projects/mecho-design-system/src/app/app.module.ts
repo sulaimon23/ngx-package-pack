1f44ea9a27f224270c042fb54b142cbd4eafeda5
Taiwo Sulaimon <tsulaimon96@gmail.com>
2023-01-10 00:01:38 +0100

    feat: finished project setup

--- projects/mecho-design-system/src/app/app.module.ts
+++ projects/mecho-design-system/src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
